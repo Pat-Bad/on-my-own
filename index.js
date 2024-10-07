247e2643612dfbfc0ea925b32c678586fb496395
Patricia <patricia.badji89@gmail.com>
2024-10-07 18:08:05 +0200

    07/10/2024

---
+++ index.js
@@ -0,0 +1,30 @@
+let numero = 5
+console.log(numero)
+const PI = 3.14
+console.log(PI)
+let nome = 'Marco'
+nome = 'Luca'
+console.log(nome)
+let saluto = 'Ciao, come stai?'
+console.log(saluto)
+let vero = true
+console.log(vero)
+
+let x = 5
+sum = x + 10
+console.log(sum)
+
+x = 9
+sum = x + 10
+console.log(sum)
+
+let name1 = 'Patricia'
+let name2 = ' Badji'
+fullname = name1 + name2
+console.log(fullname)
+
+completename = !(name1 === name2)
+console.log(completename)
+
+completename = name1 === name2
+console.log(completename)
